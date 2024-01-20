52c01628d6b10b9ec5a0d24d8ada5883846ea779
Rob Martin <rob.martin.5758@gmail.com>
2024-01-20 17:02:44 -0500

    sed 11ty for all the pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,21 @@
+const { DateTime } = require('luxon');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
+
+  eleventyConfig.addFilter('log', (value) => {
+    console.log(value);
+  });
+
+  eleventyConfig.setServerOptions({
+    // Show local network IP addresses for device testing
+    showAllHosts: true,
+  });
+
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
 
   return {
     dir: {
